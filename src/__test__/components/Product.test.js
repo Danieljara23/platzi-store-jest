e93c40d4a3a5aca3c9fd70b13e95431dd419ad15
Danieljara23 <daniel.jaramillog23@gmail.com>
2021-01-16 13:19:47 -0500

    Tests finished

---
+++ src/__test__/components/Product.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import ProductMock from '../../__mocks__/ProductMock';
+import Product from '../../components/Product';
+
+describe('<Product />', () => {
+
+  test('Product component is being render', () => {
+    const product = shallow(
+      <ProviderMock>
+        <Product />
+      </ProviderMock>,
+    );
+    expect(product.length).toEqual(1);
+  });
+
+  test('Test the buy button', () => {
+    const handleAddToCart = jest.fn();
+    const wrapper = mount(
+      <ProviderMock>
+        <Product
+          product={ProductMock}
+          handleAddToCart={handleAddToCart}
+        />
+      </ProviderMock>,
+    );
+    wrapper.find('button').simulate('click');
+    expect(handleAddToCart).toHaveBeenCalledTimes(1);
+  });
+
+});
